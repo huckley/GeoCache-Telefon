5f543ca8c0d520fe69b50eddbeec98805dc52f99
Falk Hackenberger <falk@huckley.de>
2026-05-10 15:47:23 +0200

    besserer text

--- audio/generate.sh
+++ audio/generate.sh
@@ -7,13 +7,13 @@ fi
 for i in {0..9}; do
   pico2wave --lang=de-DE -w=mp3/02/00${i}_ziffer.wav "$i"
 done
-pico2wave --lang=de-DE -w=mp3/01/001_start.wav "Hallo, Herzlich willkommen, bitte gib Deine Geheimzahlen ein!"
+pico2wave --lang=de-DE -w=mp3/01/001_start.wav "Hallo, Herzlich willkommen, bitte gib Deine Zahlen von der letzten Station ein!"
 sox -n -r 8000 -b 16 mp3/01/002_freizeichen.wav synth 600 sine 425
 pico2wave --lang=de-DE -w=mp3/01/003_ok.wav "Herzlichen Glückwunsch, Deine Geheimzahl für die nächste Station lautet:"
 pico2wave --lang=de-DE -w=mp3/01/004_fehler.wav "Das war leider falsch! Wenn Du es nochmal probieren willst, mußt Du den Hörer auflegen und dann wieder abnehmen."
 pico2wave --lang=de-DE -w=mp3/01/005_ende.wav "Vielen Dank, ich hoffe Du hattest Spaß."
 pico2wave --lang=de-DE -w=mp3/01/006_deine_Zahlen_waren.wav "Deine Zahlen waren:"
 pico2wave --lang=de-DE -w=mp3/01/007_bitte_auflegen.wav "Bitte lege den Hörer auf!"
-find ./mp3 -name "*.wav" -exec bash -c 'lame --resample 44.1 -m m -b 128 "$1" "${1%.wav}.mp3"' _ {} \;
+find ./mp3 -name "*.wav" -exec bash -c 'lame --resample 22.05 -m m -b 64 "$1" "${1%.wav}.mp3"' _ {} \;
 find ./mp3 -name "*.mp3" -exec mp3gain -r -k {} +
 find ./mp3 -name "*.wav" -delete
